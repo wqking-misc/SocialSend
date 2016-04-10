43452afc8b793e430e0bd3cc281dc5da11bc2866
NicolasDorier <nicolas.dorier@gmail.com>
2016-04-10 15:59:23 +0900

    signrawtransaction can sign P2WSH
    # Conflicts:
    #	src/script/script.cpp

--- src/script/script.h
+++ src/script/script.h
@@ -593,6 +593,7 @@ public:
     bool IsNormalPaymentScript() const;
     bool IsPayToScriptHash() const;
     
+    bool IsPayToWitnessScriptHash() const;
     bool IsWitnessProgram(int& version, std::vector<unsigned char>& program) const;
 
     /** Called by IsStandardTx and P2SH/BIP62 VerifyScript (which makes it consensus-critical). */
