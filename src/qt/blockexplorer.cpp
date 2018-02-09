eac4aa4400bc051de84d1f7656bac0c5ebc697fd
Fuzzbawls <fuzzbawls@gmail.com>
2018-02-09 02:05:03 -0800

    [Qt] Fix warning dialog popup for the Blockchain Explorer
    We have `txindex` enabled by default, but the check here was expecting
    it to be disabled by default so the warning dialog was being displayed
    informing users that they would need to enable a setting that was
    already enabled.
    
    # Conflicts:
    #	src/qt/blockexplorer.cpp

--- src/qt/blockexplorer.cpp
+++ src/qt/blockexplorer.cpp
@@ -468,7 +468,7 @@ void BlockExplorer::showEvent(QShowEvent*)
         m_History.push_back(text);
         updateNavButtons();
 
-        if (!GetBoolArg("-txindex", false)) {
+        if (!GetBoolArg("-txindex", true)) {
             QString Warning = tr("Not all transactions will be shown. To view all transactions you need to set txindex=1 in the configuration file (send.conf).");
             QMessageBox::warning(this, "SEND Core Blockchain Explorer", Warning, QMessageBox::Ok);
         }
