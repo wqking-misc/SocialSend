2994de86b96eae931d2586a954a96bb3eaa11bd0
Fuzzbawls <fuzzbawls@gmail.com>
2018-03-01 16:37:28 -0800

    [Consensus] Require standard transactions for testnet
    This brings testnet more in-line with the consensus requirements that
    are present on mainnet.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -584,7 +584,7 @@ public:
         fMiningRequiresPeers = true;
         fAllowMinDifficultyBlocks = true;
         fDefaultConsistencyChecks = false;
-        fRequireStandard = false;
+        fRequireStandard = true;
         fMineBlocksOnDemand = false;
         fTestnetToBeDeprecatedFieldRPC = true;
 
