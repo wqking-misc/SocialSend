7bc1774cb299b74779a445521e77ad741758e2f5
Julian Meyer <julianmeyer2000@gmail.com>
2018-03-16 09:12:54 -0700

    Update gitian build script to work better
    # Conflicts:
    #	contrib/gitian-build.sh

--- contrib/gitian-build.sh
+++ contrib/gitian-build.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Copyright (c) 2016 The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
@@ -6,7 +7,6 @@
 sign=false
 verify=false
 build=false
-setupenv=false
 
 # Systems to build
 linux=true
@@ -28,7 +28,7 @@ signProg="gpg --detach-sign"
 commitFiles=true
 
 # Help Message
-read -d '' usage <<- EOF
+read -r -d '' usage <<- EOF
 Usage: $scriptName [-c|u|v|b|s|B|o|h|j|m|] signer version
 
 Run this script from the directory containing the send, gitian-builder, gitian.sigs, and send-detached-sigs.
@@ -111,7 +111,7 @@ while :; do
 		fi
 		shift
 	    else
-		echo 'Error: "--os" requires an argument containing an l (for linux), w (for windows), x (for Mac OSX), or a (for aarch64)\n'
+		printf 'Error: "--os" requires an argument containing an l (for linux), w (for windows), x (for Mac OSX), or a (for aarch64)\n'
 		exit 1
 	    fi
 	    ;;
@@ -196,7 +196,7 @@ then
 fi
 
 # Get signer
-if [[ -n"$1" ]]
+if [[ -n "$1" ]]
 then
     SIGNER=$1
     shift
@@ -231,7 +231,7 @@ if [[ $commit = false ]]
 then
 	COMMIT="v${VERSION}"
 fi
-echo ${COMMIT}
+echo "${COMMIT}"
 
 # Setup build environment
 if [[ $setup = true ]]
@@ -240,7 +240,7 @@ then
     git clone https://github.com/send-project/gitian.sigs.git
     git clone https://github.com/send-project/send-detached-sigs.git
     git clone https://github.com/devrandom/gitian-builder.git
-    pushd ./gitian-builder
+    pushd ./gitian-builder || exit
     if [[ -n "$USE_LXC" ]]
     then
         sudo apt-get install lxc
@@ -248,30 +248,30 @@ then
     else
         bin/make-base-vm --suite trusty --arch amd64
     fi
-    popd
+    popd || exit
 fi
 
 # Set up build
-pushd ./send
+pushd ./send || exit
 git fetch
-git checkout ${COMMIT}
-popd
+git checkout "${COMMIT}"
+popd || exit
 
 # Build
 if [[ $build = true ]]
 then
 	# Make output folder
-	mkdir -p ./send-binaries/${VERSION}
+	mkdir -p "./send-binaries/${VERSION}"
 
 	# Build Dependencies
 	echo ""
 	echo "Building Dependencies"
 	echo ""
-	pushd ./gitian-builder
+	pushd ./gitian-builder || exit
 	mkdir -p inputs
 	wget -N -P inputs $osslPatchUrl
 	wget -N -P inputs $osslTarUrl
-	make -C ../send/depends download SOURCES_PATH=`pwd`/cache/common
+	make -C ../send/depends download SOURCES_PATH="$(pwd)/cache/common"
 
 	# Linux
 	if [[ $linux = true ]]
@@ -280,7 +280,7 @@ then
 	    echo "Compiling ${VERSION} Linux"
 	    echo ""
 	    ./bin/gbuild -j ${proc} -m ${mem} --commit send=${COMMIT} --url send=${url} ../send/contrib/gitian-descriptors/gitian-linux.yml
-	    ./bin/gsign -p $signProg --signer $SIGNER --release ${VERSION}-linux --destination ../gitian.sigs/ ../send/contrib/gitian-descriptors/gitian-linux.yml
+	    ./bin/gsign --signer $SIGNER --release ${VERSION}-linux --destination ../gitian.sigs/ ../send/contrib/gitian-descriptors/gitian-linux.yml
 	    mv build/out/send-*.tar.gz build/out/src/send-*.tar.gz ../send-binaries/${VERSION}
 	fi
 	# Windows
@@ -290,7 +290,7 @@ then
 	    echo "Compiling ${VERSION} Windows"
 	    echo ""
 	    ./bin/gbuild -j ${proc} -m ${mem} --commit send=${COMMIT} --url send=${url} ../send/contrib/gitian-descriptors/gitian-win.yml
-	    ./bin/gsign -p $signProg --signer $SIGNER --release ${VERSION}-win-unsigned --destination ../gitian.sigs/ ../send/contrib/gitian-descriptors/gitian-win.yml
+	    ./bin/gsign --signer $SIGNER --release ${VERSION}-win-unsigned --destination ../gitian.sigs/ ../send/contrib/gitian-descriptors/gitian-win.yml
 	    mv build/out/send-*-win-unsigned.tar.gz inputs/send-win-unsigned.tar.gz
 	    mv build/out/send-*.zip build/out/send-*.exe ../send-binaries/${VERSION}
 	fi
@@ -301,7 +301,7 @@ then
 	    echo "Compiling ${VERSION} Mac OSX"
 	    echo ""
 	    ./bin/gbuild -j ${proc} -m ${mem} --commit send=${COMMIT} --url send=${url} ../send/contrib/gitian-descriptors/gitian-osx.yml
-	    ./bin/gsign -p $signProg --signer $SIGNER --release ${VERSION}-osx-unsigned --destination ../gitian.sigs/ ../send/contrib/gitian-descriptors/gitian-osx.yml
+	    ./bin/gsign --signer $SIGNER --release ${VERSION}-osx-unsigned --destination ../gitian.sigs/ ../send/contrib/gitian-descriptors/gitian-osx.yml
 	    mv build/out/send-*-osx-unsigned.tar.gz inputs/send-osx-unsigned.tar.gz
 	    mv build/out/send-*.tar.gz build/out/send-*.dmg ../send-binaries/${VERSION}
 	fi
@@ -312,7 +312,7 @@ then
 	    echo "Compiling ${VERSION} AArch64"
 	    echo ""
 	    ./bin/gbuild -j ${proc} -m ${mem} --commit send=${COMMIT} --url send=${url} ../send/contrib/gitian-descriptors/gitian-aarch64.yml
-	    ./bin/gsign -p $signProg --signer $SIGNER --release ${VERSION}-aarch64 --destination ../gitian.sigs/ ../send/contrib/gitian-descriptors/gitian-aarch64.yml
+	    ./bin/gsign --signer $SIGNER --release ${VERSION}-aarch64 --destination ../gitian.sigs/ ../send/contrib/gitian-descriptors/gitian-aarch64.yml
 	    mv build/out/send-*.tar.gz build/out/src/send-*.tar.gz ../send-binaries/${VERSION}
 	popd
 
@@ -322,13 +322,13 @@ then
             echo ""
             echo "Committing ${VERSION} Unsigned Sigs"
             echo ""
-            pushd gitian.sigs
+            pushd gitian.sigs || exit
             git add ${VERSION}-linux/${SIGNER}
             git add ${VERSION}-aarch64/${SIGNER}
             git add ${VERSION}-win-unsigned/${SIGNER}
             git add ${VERSION}-osx-unsigned/${SIGNER}
             git commit -a -m "Add ${VERSION} unsigned sigs for ${SIGNER}"
-            popd
+            popd || exit
         fi
 fi
 
@@ -336,7 +336,7 @@ fi
 if [[ $verify = true ]]
 then
 	# Linux
-	pushd ./gitian-builder
+	pushd ./gitian-builder || exit
 	echo ""
 	echo "Verifying v${VERSION} Linux"
 	echo ""
@@ -366,14 +366,14 @@ then
 	echo "Verifying v${VERSION} Signed Mac OSX"
 	echo ""
 	./bin/gverify -v -d ../gitian.sigs/ -r ${VERSION}-osx-signed ../send/contrib/gitian-descriptors/gitian-osx-signer.yml
-	popd
+	popd || exit
 fi
 
 # Sign binaries
 if [[ $sign = true ]]
 then
 
-        pushd ./gitian-builder
+        pushd ./gitian-builder || exit
 	# Sign Windows
 	if [[ $windows = true ]]
 	then
@@ -381,7 +381,7 @@ then
 	    echo "Signing ${VERSION} Windows"
 	    echo ""
 	    ./bin/gbuild -i --commit signature=${COMMIT} ../send/contrib/gitian-descriptors/gitian-win-signer.yml
-	    ./bin/gsign -p $signProg --signer $SIGNER --release ${VERSION}-win-signed --destination ../gitian.sigs/ ../send/contrib/gitian-descriptors/gitian-win-signer.yml
+	    ./bin/gsign --signer $SIGNER --release ${VERSION}-win-signed --destination ../gitian.sigs/ ../send/contrib/gitian-descriptors/gitian-win-signer.yml
 	    mv build/out/send-*win64-setup.exe ../send-binaries/${VERSION}
 	    mv build/out/send-*win32-setup.exe ../send-binaries/${VERSION}
 	fi
@@ -392,21 +392,21 @@ then
 	    echo "Signing ${VERSION} Mac OSX"
 	    echo ""
 	    ./bin/gbuild -i --commit signature=${COMMIT} ../send/contrib/gitian-descriptors/gitian-osx-signer.yml
-	    ./bin/gsign -p $signProg --signer $SIGNER --release ${VERSION}-osx-signed --destination ../gitian.sigs/ ../send/contrib/gitian-descriptors/gitian-osx-signer.yml
+	    ./bin/gsign --signer $SIGNER --release ${VERSION}-osx-signed --destination ../gitian.sigs/ ../send/contrib/gitian-descriptors/gitian-osx-signer.yml
 	    mv build/out/send-osx-signed.dmg ../send-binaries/${VERSION}/send-${VERSION}-osx.dmg
 	fi
-	popd
+	popd || exit
 
         if [[ $commitFiles = true ]]
         then
             # Commit Sigs
-            pushd gitian.sigs
+            pushd gitian.sigs || exit
             echo ""
             echo "Committing ${VERSION} Signed Sigs"
             echo ""
             git add ${VERSION}-win-signed/${SIGNER}
             git add ${VERSION}-osx-signed/${SIGNER}
             git commit -a -m "Add ${VERSION} signed binary sigs for ${SIGNER}"
-            popd
+            popd || exit
         fi
 fi
