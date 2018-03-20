ff231863a23e68941a76fe26aea200d4fc17d282
Fuzzbawls <fuzzbawls@gmail.com>
2018-03-20 00:53:27 -0700

    Clean up apparent locking issues
    These locks address compiler warnings when compiling with clang's
    `-Wthread-safety-analysis`

--- src/main.cpp
+++ src/main.cpp
@@ -5476,6 +5476,7 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
         if (pfrom->nVersion < CADDR_TIME_VERSION && addrman.size() > 1000)
             return true;
         if (vAddr.size() > 1000) {
+            LOCK(cs_main);
             Misbehaving(pfrom->GetId(), 20);
             return error("message addr size() = %u", vAddr.size());
         }
