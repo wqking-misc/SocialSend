f18f9e8f5646b3443d4ae38361a95001a775ca8f
Julian Meyer <julianmeyer2000@gmail.com>
2018-04-18 14:19:26 -0700

    Update testnet params # Conflicts: #	src/chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -538,11 +538,12 @@ public:
         nMinerThreads = 0;
         nTargetTimespan = 1 * 60 * 40;  // SEND: 40 minutes
         nTargetSpacing = 1 * 60;        // SEND: 1 minute
-        nLastPOWBlock = 200;
         nMaturity = 15;
         nMasternodeCountDrift = 4;
         nModifierUpdateBlock = 51197; //approx Mon, 17 Apr 2017 04:00:00 GMT
         nMaxMoneyOut = 43199500 * COIN;
+        nLastPOWBlock = 200;
+        nZerocoinStartHeight = 201576;
 
         //! Modify the testnet genesis block so the timestamp is valid for a later start.
         genesis.nTime = 1454124731;
