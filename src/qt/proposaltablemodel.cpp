2b70d187b0016fdb01c7a69fda441ac84c6bccb4
wqking <wqking@outlook.com>
2021-04-12 13:57:59 +0800

    Fixed compile error

--- src/qt/proposaltablemodel.cpp
+++ src/qt/proposaltablemodel.cpp
@@ -173,7 +173,7 @@ QVariant ProposalTableModel::data(const QModelIndex &index, int role) const
         case Percentage:
             return QString("%1\%").arg(rec->percentage);
         case Amount:
-            return BitcoinUnits::format(BitcoinUnits::PHR, rec->amount);
+            return BitcoinUnits::format(BitcoinUnits::SEND, rec->amount);
         }
         break;
     case Qt::EditRole:
